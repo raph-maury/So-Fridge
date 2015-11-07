e40f318d5444f679860a4009213d91a457171b08
Raphael Maury <raphael.maury@gmail.com>
2015-11-07 13:54:48 +0100

    Initial Commit

---
+++ SoFridge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoFridge
+//
+//  Created by Raphael MAURY on 07/11/2015.
+//  Copyright © 2015 Raphael MAURY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
